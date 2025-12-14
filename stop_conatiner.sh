d6ed2e1ce975d4526c729b3a9cf30914f17d0ea1
Manju Goswami <manju.goswami2101@gmail.com>
2025-12-14 15:29:23 +0530

    stop container

--- stop_conatiner.sh
+++ stop_conatiner.sh
@@ -2,4 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-echo "Hi"
+containerid = ' docker ps | awk -f "" '{printf $1}'' #this will get the contaienr id of the running container
+docker rm -f $containerid            # this will force delete the running container
